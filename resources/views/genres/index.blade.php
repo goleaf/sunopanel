bbf1f8a35a56b398084f3dbdaa407d439bb55a6d
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-19 15:09:30 +0300

    Additional button style standardization in genre views

--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -8,18 +8,14 @@
     <div class="container mx-auto px-4">
         @if (session('success'))
             <div class="alert alert-success mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                <x-icon name="check" size="6" />
                 <span>{{ session('success') }}</span>
             </div>
         @endif
 
         @if (session('error'))
             <div class="alert alert-error mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                <x-icon name="x" size="6" />
                 <span>{{ session('error') }}</span>
             </div>
         @endif
@@ -30,21 +26,17 @@
                     <div class="w-full md:w-1/2">
                         <form method="GET" action="{{ route('genres.index') }}" class="join">
                             <input type="text" name="search" placeholder="Search genres..." value="{{ request('search') }}" class="input input-bordered join-item w-full" />
-                            <button type="submit" class="btn btn-primary join-item">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
+                            <x-button type="submit" color="primary" class="join-item">
+                                <x-icon name="search" size="5" class="mr-1" />
                                 Search
-                            </button>
+                            </x-button>
                         </form>
                     </div>
                     <div>
-                        <a href="{{ route('genres.create') }}" class="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                            </svg>
+                        <x-button href="{{ route('genres.create') }}" color="primary">
+                            <x-icon name="plus" size="5" class="mr-1" />
                             {{ __('Create Genre') }}
-                        </a>
+                        </x-button>
                     </div>
                 </div>
 
@@ -59,13 +51,7 @@
                                     ])) }}" class="flex items-center">
                                         {{ __('Name') }}
                                         @if(request('sort') === 'name')
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                @if(request('direction') === 'asc')
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                                                @else
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                                                @endif
-                                            </svg>
+                                            <x-icon name="{{ request('direction') === 'asc' ? 'chevron-up' : 'chevron-down' }}" size="4" class="ml-1" />
                                         @endif
                                     </a>
                                 </th>
@@ -76,13 +62,7 @@
                                     ])) }}" class="flex items-center">
                                         {{ __('Tracks Count') }}
                                         @if(request('sort') === 'tracks_count')
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                @if(request('direction') === 'asc')
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                                                @else
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                                                @endif
-                                            </svg>
+                                            <x-icon name="{{ request('direction') === 'asc' ? 'chevron-up' : 'chevron-down' }}" size="4" class="ml-1" />
                                         @endif
                                     </a>
                                 </th>
@@ -93,13 +73,7 @@
                                     ])) }}" class="flex items-center">
                                         {{ __('Created At') }}
                                         @if(request('sort') === 'created_at')
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                @if(request('direction') === 'asc')
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                                                @else
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                                                @endif
-                                            </svg>
+                                            <x-icon name="{{ request('direction') === 'asc' ? 'chevron-up' : 'chevron-down' }}" size="4" class="ml-1" />
                                         @endif
                                     </a>
                                 </th>
@@ -122,37 +96,30 @@
                                     </td>
                                     <td>
                                         <div class="flex flex-col md:flex-row gap-2">
-                                            <a href="{{ route('genres.show', $genre) }}" class="btn btn-info btn-xs">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                                </svg>
+                                            <x-button href="{{ route('genres.show', $genre) }}" color="info" size="xs">
+                                                <x-icon name="eye" size="4" class="mr-1" />
                                                 View
-                                            </a>
-                                            <a href="{{ route('genres.edit', $genre) }}" class="btn btn-warning btn-xs">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                </svg>
+                                            </x-button>
+                                            
+                                            <x-button href="{{ route('genres.edit', $genre) }}" color="warning" size="xs">
+                                                <x-icon name="pencil" size="4" class="mr-1" />
                                                 Edit
-                                            </a>
+                                            </x-button>
+                                            
                                             <form method="POST" action="{{ route('genres.destroy', $genre) }}" class="inline-block">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" 
-                                                    class="btn btn-error btn-xs" 
+                                                <x-button type="submit" color="error" size="xs" 
                                                     onclick="return confirm('{{ __('Are you sure you want to delete this genre?') }}')">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                    </svg>
+                                                    <x-icon name="trash" size="4" class="mr-1" />
                                                     Delete
-                                                </button>
+                                                </x-button>
                                             </form>
-                                            <a href="{{ route('playlists.create-from-genre', $genre) }}" class="btn btn-accent btn-xs">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-                                                </svg>
+                                            
+                                            <x-button href="{{ route('playlists.create-from-genre', $genre) }}" color="accent" size="xs">
+                                                <x-icon name="music" size="4" class="mr-1" />
                                                 {{ __('Create Playlist') }}
-                                            </a>
+                                            </x-button>
                                         </div>
                                     </td>
                                 </tr>
--- resources/views/genres/show.blade.php
+++ resources/views/genres/show.blade.php
@@ -5,30 +5,24 @@
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-3xl font-bold text-primary">{{ $genre->name }}</h1>
         <div class="flex gap-2">
-            <a href="{{ route('genres.edit', $genre->id) }}" class="btn btn-primary btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
+            <x-button href="{{ route('genres.edit', $genre->id) }}" color="primary" size="sm">
+                <x-icon name="pencil" size="5" class="mr-1" />
                 Edit Genre
-            </a>
+            </x-button>
             
             <form action="{{ route('playlists.create-from-genre', $genre->id) }}" method="POST" class="inline-block">
                 @csrf
-                <button type="submit" class="btn btn-success btn-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-                    </svg>
+                <x-button type="submit" color="success" size="sm">
+                    <x-icon name="music" size="5" class="mr-1" />
                     Create Playlist
-                </button>
+                </x-button>
             </form>
         </div>
     </div>
 
     @if(session('success'))
         <div class="alert alert-success mb-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
+            <x-icon name="check" size="6" />
             <span>{{ session('success') }}</span>
         </div>
     @endif
@@ -49,9 +43,7 @@
                                     <div class="flex items-center">
                                         Track
                                         <a href="{{ route('genres.show', ['genre' => $genre->id, 'sort' => 'name', 'order' => ($sortField == 'name' && $sortOrder == 'asc') ? 'desc' : 'asc']) }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
-                                            </svg>
+                                            <x-icon name="chevron-down" size="4" class="ml-1" />
                                         </a>
                                     </div>
                                 </th>
@@ -59,9 +51,7 @@
                                     <div class="flex items-center">
                                         Duration
                                         <a href="{{ route('genres.show', ['genre' => $genre->id, 'sort' => 'duration', 'order' => ($sortField == 'duration' && $sortOrder == 'asc') ? 'desc' : 'asc']) }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
-                                            </svg>
+                                            <x-icon name="chevron-down" size="4" class="ml-1" />
                                         </a>
                                     </div>
                                 </th>
@@ -84,17 +74,12 @@
                                     <td>{{ $track->duration ?? '0:00' }}</td>
                                     <td>
                                         <div class="flex gap-2 justify-end">
-                                            <a href="{{ route('tracks.show', $track) }}" class="btn btn-circle btn-sm btn-ghost">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                                                </svg>
-                                            </a>
-                                            <a href="{{ route('tracks.play', $track) }}" class="btn btn-circle btn-sm btn-ghost text-success">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                                                </svg>
-                                            </a>
+                                            <x-button href="{{ route('tracks.show', $track) }}" color="ghost" size="sm" icon>
+                                                <x-icon name="eye" size="5" />
+                                            </x-button>
+                                            <x-button href="{{ route('tracks.play', $track) }}" color="success" size="sm" icon>
+                                                <x-icon name="play" size="5" />
+                                            </x-button>
                                         </div>
                                     </td>
                                 </tr>
@@ -139,12 +124,10 @@
     </div>
 
     <div class="mt-6">
-        <a href="{{ route('genres.index') }}" class="btn btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-            </svg>
+        <x-button href="{{ route('genres.index') }}" color="ghost">
+            <x-icon name="back" size="5" class="mr-2" />
             Back to Genres
-        </a>
+        </x-button>
     </div>
 </div>
 @endsection
