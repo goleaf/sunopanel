47ae997df54be76278bb490111b98a1485e6c12a
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-19 14:36:35 +0300

    Make UserController final and add strict typing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
+use App\Http\Requests\UserUpdateRequest;
 
-class UserController extends Controller
+final class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
