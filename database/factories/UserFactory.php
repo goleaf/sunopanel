f60d42a078f3975307f541d1ebea67bd03b4a878
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-21 14:48:09 +0300

    Fix hash verification error by using plain text password in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // Plain text password will be hashed by model cast
             'remember_token' => Str::random(10),
         ];
     }
