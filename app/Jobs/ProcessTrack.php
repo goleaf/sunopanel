47214e16436f2541451c5cae2032256a8fdc33be
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-21 22:39:09 +0300

    Add automatic retry with 5-second backoff for failed ProcessTrack jobs

--- app/Jobs/ProcessTrack.php
+++ app/Jobs/ProcessTrack.php
@@ -23,6 +23,20 @@ class ProcessTrack implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 3;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int
+     */
+    public $backoff = 5;
+
+    /**
      * The track to process.
      *
      * @var Track
